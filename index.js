17f11b3b4ed418f6b25f0559b3cabfa7d21fa9dd
tahsin-99 <somethinglearn53@gmail.com>
2025-12-09 00:37:21 +0600

    packages-data-done

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
    
     const db=client.db('assetverse');
+    const packageCollections=db.collection('packages')
+
+    // package api
+
+app.get('/packages',async(req,res)=>{
+  const result=await packageCollections.find().toArray()
+  res.send(result)
+})
+
      
 
     // Send a ping to confirm a successful connection
@@ -41,6 +50,8 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('VerseAsset..')
 })
+
+
 
 
 
