83c17f9dbd65b02bb91e19be039c3190058a1181
tahsin-99 <somethinglearn53@gmail.com>
2025-12-09 14:25:28 +0600

    add-users-done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT ||3000
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(express.json());
@@ -12,40 +11,48 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@t-mongo.m4mnwdk.mongodb.net/?appName=T-mongo`;
 
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-   
-    const db=client.db('assetverse');
-    const packageCollections=db.collection('packages')
-    const featuresCollections=db.collection('features')
+
+    const db = client.db("assetverse");
+    const packageCollections = db.collection("packages");
+    const featuresCollections = db.collection("features");
+    const usersCollections = db.collection("users");
+
+    // users api
+
+    app.post('/users',async(req,res)=>{
+      const userInfo=req.body;
+      const result=await usersCollections.insertOne(userInfo)
+      res.send(result)
+    })
 
     // package api
 
-app.get('/packages',async(req,res)=>{
-  const result=await packageCollections.find().toArray()
-  res.send(result)
-})
-// feature apis
-app.get('/features',async(req,res)=>{
-  const result=await featuresCollections.find().toArray()
-  res.send(result)
-})
-
-     
+    app.get("/packages", async (req, res) => {
+      const result = await packageCollections.find().toArray();
+      res.send(result);
+    });
+    // feature apis
+    app.get("/features", async (req, res) => {
+      const result = await featuresCollections.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,14 +60,10 @@ app.get('/features',async(req,res)=>{
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('VerseAsset..')
-})
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("VerseAsset..");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
